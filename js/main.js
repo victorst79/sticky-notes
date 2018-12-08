1e33094c075d16633f5f514c7d9cd38467c9b987
unknown <victorst79@gmail.com>
2018-12-08 22:22:54 +0100

    rework all structure exercise

--- js/main.js
+++ js/main.js
@@ -4,37 +4,9 @@ import {Note} from './note.js';
  * 
  * @param {Objects} Note 
 **/
-function printNote(Note){
-    var noteElement = document.createElement("div");
-    var titleElement = document.createElement("h3");
-    var textElement = document.createElement("p");
-    var dateElement = document.createElement("small");
+function createNote(){
+    let title = document.getElementById("title").value;
+    let text = document.getElementById("text").value;
 
-    titleElement.innerText = Note.title;
-    textElement.innerText = Note.text;
-    dateElement.innerText = Note.date;
-
-    noteElement.append(titleElement);
-    noteElement.append(textElement);
-    noteElement.append(dateElement);
-    noteElement.setAttribute("class","note");
-    
-    return noteElement;
+    var note = new Note(title,text);
 }
-
-function initialNotes(elementID){
-    // var notas = [new Note("Title","Lorem ipsum"),new Note("Title","Lorem ipsum"),new Note("Title","Lorem ipsum"),new Note("Title","Lorem ipsum")];
-
-    // for(let i = 0; i < notas.length; i++){       
-    //     document.getElementById(elementID).append(printNote(notas[i]));
-    // }
-
-    // var title = document.getElementById("title").value;
-    // var text = document.getElementById("text").value;
-    // var note = new Note(title,text);
-    // console.log(note);
-}
-document.getElementById("submit").addEventListener("onclick",printNote(note));
-
-
-document.onload = initialNotes("note-board");
